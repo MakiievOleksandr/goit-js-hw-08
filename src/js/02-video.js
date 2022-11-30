e77d74c960a34fd7aa79d01d9dc29fb98bb4d958
MakiievOleksandr <alsermak@gmail.com>
2022-11-30 21:53:42 +0200

    js HW 08. v1.0

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeSet, 1000));
+
+function onTimeSet(evt) {
+  const localData = localStorage.setItem(
+    'videoplayer-current-time',
+    evt.seconds
+  );
+}
+
+const parsedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(parsedTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const inputEmailEl = document.querySelector('input[name="email"]');
+const inputTextEl = document.querySelector('textarea[name="message"]');
+
+checkStorage();
+
+const formInputData = {};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onSubmitClick);
+
+function onFormInput(evt) {
+  formInputData[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formInputData));
+}
+
+function onSubmitClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.email.value !== '' && evt.target.message.value !== '') {
+    evt.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+    console.log(formInputData);
+  }
+}
+
+function checkStorage() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  const parsedData = JSON.parse(savedData);
+
+  if (savedData) {
+    inputEmailEl.value = parsedData.email;
+    inputTextEl.value = parsedData.message;
+  }
+}
